357f83745b6a50e2f65da365872db3d500147856
aidynsemey7sk <aidynsemey7sk@gmail.com>
2024-02-16 17:02:41 +0600

    add router

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from .views import ListPost
 
 
 urlpatterns = [
-    path('', ListPost.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,11 +3,6 @@ from blog.models import Post
 from .serializers import PostSerializer
 
 
-class ListPost(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
